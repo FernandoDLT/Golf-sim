6c9d54df2df4db01a6baba7e48e487d023945ab6
FERNANDO DE LA TORRE <96254499+FernandoDLT@users.noreply.github.com>
2024-08-26 17:51:17 -0600

    Adjusted intro text padding

--- public/main.js
+++ public/main.js
@@ -15,24 +15,24 @@ document.getElementById('new-round').addEventListener('click', handleNewRound);
 
 // Define holes array
 const holes = [
-    { number: 1, par: 3, distance: 100 },
-    { number: 2, par: 3, distance: 120 },
-    { number: 3, par: 4, distance: 410 },
+    { number: 1, par: 4, distance: 400 },
+    { number: 2, par: 4, distance: 380 },
+    { number: 3, par: 3, distance: 180 },
     { number: 4, par: 5, distance: 530 },
-    { number: 5, par: 3, distance: 210 },
-    { number: 6, par: 4, distance: 420 },
-    { number: 7, par: 3, distance: 200 },
-    { number: 8, par: 5, distance: 550 },
-    { number: 9, par: 4, distance: 430 },
-    { number: 10, par: 4, distance: 380 },
-    { number: 11, par: 3, distance: 160 },
-    { number: 12, par: 5, distance: 510 },
-    { number: 13, par: 4, distance: 440 },
-    { number: 14, par: 4, distance: 340 },
-    { number: 15, par: 3, distance: 200 },
-    { number: 16, par: 5, distance: 540 },
-    { number: 17, par: 4, distance: 410 },
-    { number: 18, par: 4, distance: 390 }
+    { number: 5, par: 4, distance: 410 },
+    { number: 6, par: 4, distance: 390 },
+    { number: 7, par: 3, distance: 150 },
+    { number: 8, par: 5, distance: 560 },
+    { number: 9, par: 4, distance: 420 },
+    { number: 10, par: 4, distance: 430 },
+    { number: 11, par: 4, distance: 350 },
+    { number: 12, par: 5, distance: 580 },
+    { number: 13, par: 3, distance: 210 },
+    { number: 14, par: 4, distance: 410 },
+    { number: 15, par: 4, distance: 370 },
+    { number: 16, par: 3, distance: 160 },
+    { number: 17, par: 5, distance: 520 },
+    { number: 18, par: 4, distance: 440 }
 ];
 
 // Function to check if all fields are filled
